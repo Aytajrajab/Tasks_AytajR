9a099a0d84f0863cd6ef1002f22ad274f9ff5e89
Aytajrajab <88050466+Aytajrajab@users.noreply.github.com>
2021-10-21 15:01:48 +0400

    Add files via upload

---
+++ Array of consecutive/js/script.js
@@ -0,0 +1,10 @@
+let arr = [];
+let num1 = Number(prompt("Enter start number:"));
+let num2 = Number(prompt("Enter end number:"));
+
+
+    while (num1<=num2) {
+        arr.push(num1++);
+    }
+    console.log(arr);
+    
